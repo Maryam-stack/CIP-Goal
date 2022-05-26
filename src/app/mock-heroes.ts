030fb18bfcd6928baf83a73a99954fb487d1de9e
maryam-stack <maryamfatima2k@gmail.com>
2022-05-26 17:16:51 +0500

    demo

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import {Hero} from './heroes';
+
+export const HEROES: Hero[]= [
+    {id : 1, name : 'Quaid.e.Azam'},
+    {id: 2, name: 'Allama Iqbal'},
+    {id: 3, name: 'Abdul Sattar Edhi'},
+    {id: 4, name: 'Fatima Jinnah'},
+    {id: 5, name: 'Sir syed Ahmed Khan'}
+];
